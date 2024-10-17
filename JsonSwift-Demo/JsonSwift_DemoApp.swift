15ccdf9bd0965553c73479ad68c145a84a7c2eb9
Denidu Gamage <denidugamage21@gmail.com>
2024-10-17 10:22:12 +0530

    Initial Commit

---
+++ JsonSwift-Demo/JsonSwift_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  JsonSwift_DemoApp.swift
+//  JsonSwift-Demo
+//
+//  Created by Denidu Gamage on 2024-10-17.
+//
+
+import SwiftUI
+
+@main
+struct JsonSwift_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
